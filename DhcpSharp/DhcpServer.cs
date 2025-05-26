486c3ab84fba6e8b1db2b2b1b548df29904b76a4
Kris <krismakesrandomstuff@gmail.com>
2025-05-26 20:25:56 +0200

    Working on DHCP server and logger

---
+++ DhcpSharp/DhcpServer.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace DhcpSharp
+{
+    public class DhcpServer
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+
+        public DhcpServer(string ip, int port) {
+            this.Ip = ip;
+            this.Port = port;
+        }
+
+        public void Start() {
+            IPEndPoint local = new(IPAddress.Parse(this.Ip), this.Port);
+            UdpClient udp = new(local);
+            
+
+        }
+    }
+}
---
+++ DhcpSharp/Logger/ConsoleLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DhcpSharp.Logger
+{
+    public class ConsoleLogger : ILogger {
+        public void LogError(string msg) {
+
+        }
+        public void LogInfo(string msg) {
+
+        }
+        public void LogWarning(string msg) {
+
+        }
+    }
+}
